4a944712a3c8904367de23931ea11a19a570dc3b
songh_work <songh-cc@foxmail.com>
2022-03-30 15:34:19 +0800

    2024题做出来了，但是有点暴力，特别长的字符串超时了

--- MyLeetCode/Solution2024.cpp
+++ MyLeetCode/Solution2024.cpp
@@ -3,65 +3,33 @@
 
 int Solution2024::maxConsecutiveAnswers(std::string answerKey, int k) {
 	
-	int origin_k = k;
-	unsigned int result = 0;
 	// 不能用 char letter : "TF"  这样会循环三次的，这是C风格的字符串，后面还有一个标志着结束的字符
 	std::string loop_chars = "TF";
 	// 用T算一次最大，再用F算一次最大，作比较
-	for (char letter : loop_chars)  { 
+	int origin_k = k;  // 备份最开始的k值
+	unsigned int result = 0;   // 结果
 
-		bool flag = true;   // 这是为了处理answerKey = "TFFT"; k=1这样的数据，最后会执行k = origin_k;，导致下面多加一个k，所以增加一个flag
-
-		unsigned int max_t = 0, temp_max_t = 0;
-		for (char c : answerKey) {
-
-			flag = true;  // 保证每次为tru，只管最后一次是否进到了k==0的判定，进去了就置为false
-
-			if (c == letter) {
-				temp_max_t += 1;
-			}
-			else {
-				if (max_t < temp_max_t) {
-					max_t = temp_max_t;
-				}
-				if (k == 0) {
-					// k等于0后，重置k和暂时最大的值都为0，相当于重新开始，上面也把暂时最大的值记录了：
-					temp_max_t = 0;
-					k = origin_k;
-					flag = false;
-					continue;
-				}
-
-				if (k != 0) {
-					temp_max_t += 1;
+	for (char letter : loop_chars) {
+		for (size_t i = 0; i < answerKey.size(); ++i) {
+			unsigned temp_max = 0;  // 每向右移动一次后，要重置这个数
+			// 不但每次循环 T、F时，都要把 k 还原成原来的值，向右移动后，也要重置k
+			k = origin_k;
+			for (std::string::const_iterator iter = answerKey.cbegin() + i; iter != answerKey.cend(); ++iter) {
+				if (*iter == letter)
+					++temp_max;
+				else if (k != 0) {
+					++temp_max;
 					--k;
 				}
-				if (max_t < temp_max_t) {
-					max_t = temp_max_t;
+				else {
+					if (result < temp_max)
+						result = temp_max;
+					break;  // 此时就退出循环，再从第二位开始
 				}
+				if (result < temp_max)
+					result = temp_max;   // 如果没进去else，也要去对比
 			}
 		}
-		if (max_t < temp_max_t) {
-			max_t = temp_max_t;
-		}
-		if (result < max_t) {
-			result = max_t;
-		}
-		
-		// 循环完了，k可能还没用完，有几个k就能变几个值，就直接加到result中
-		// 同时这个k还不能是 k = origin_k; 获得的k
-		std::cout << k  << std::endl;
-		std::cout << flag  << std::endl;
-		std::cout << result  << std::endl;
-		std::cout << "***********" << std::endl;
-		if (k != 0 && flag) {
-			result += k;
-		}
-	}
-
-	// 因为上面加k，可能导致超出原来字符串的长度
-	if (result > answerKey.size()) {
-		result = answerKey.size();
 	}
 	return result;
 }
--- MyLeetCode/main.cpp
+++ MyLeetCode/main.cpp
@@ -19,32 +19,9 @@ bool func693() {
 }
 
 int func2024() {
-	std::string answerKey = "TTFTTFTT";
-	int k = 1;
-
-	unsigned max_t = 0;
-	for (char c : answerKey) {
-		if (c == 'T') {
-			++max_t;
-		}
-		else {
-			if (k != 0) {
-				++max_t;
-				--k;
-			}
-		}
-	}
-
-
-
-
-
-
-
-
-	//std::string answerKey = "FFFTTFTTFT";
-	//int k = 3;
-
+	// 算法基本没问题了，但是很长的那个字符串，leetcode上超时了
+	std::string answerKey = "FFFTTFTTFT";
+	int k = 3;
 	Solution2024 solution;
 	int out = solution.maxConsecutiveAnswers(answerKey, k);
 	return out;
@@ -56,50 +33,8 @@ int main(int argc, char** argv) {
 
 	int out = func2024();
 
-	//std::string answerKey = "TTFTTFTT";
-	//int k = 1;
-
-	//int origin_k = k;
-	//unsigned int res = 0;
-	//for (char letter : "TF") {
-	//	
-	//	unsigned int max_t = 0, temp_max_t = 0;
-	//	for (char c : answerKey) {
-	//		if (c == letter) {
-	//			temp_max_t += 1;
-	//		}
-	//		else {
-	//			if (max_t < temp_max_t) {
-	//				max_t = temp_max_t;
-	//			}
-	//			if (k == 0) {
-	//				// k等于0后，重置：
-	//				temp_max_t = 0;
-	//				k = origin_k;
-	//				continue;
-	//			}
-
-	//			if (k != 0) {
-	//				temp_max_t += 1;
-	//				--k;
-	//			}
-	//			if (max_t < temp_max_t) {
-	//				max_t = temp_max_t;
-	//			}
-	//		}
-	//	}
-	//	if (max_t < temp_max_t) {
-	//		max_t = temp_max_t;
-	//	}
-	//	if (res < max_t) {
-	//		res = max_t;
-	//	}
-	//}
-
-	
 
 	std::cout << out << std::endl;
-
 	system("pause");
 	return 0;
 }
